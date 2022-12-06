f280f439d26c9e00ce7debe860e8632ac4140d7b
Karthik-nimmani <118656008+Karthik-nimmani@users.noreply.github.com>
2022-12-06 20:58:21 +0530

    mini project

---
+++ Email Slicer.py
@@ -0,0 +1,8 @@
+mail=lambda a,b:(a-b)+b
+while True:
+    a=input('Enter a valid e-mail adress:-')
+    if '@' in a:
+        a=a.split('@')
+        print('Username:- {} and Domain:- {}'.format(a[0],a[1]))
+    else:
+        print('E-mail is not Valid')
